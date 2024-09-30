61db9af710e2124f57f15f8519f745b4a6205d2e
AnastasiyaKuzmina <anastasiya.kuzmina.0202@mail.ru>
2024-09-30 14:52:35 +0300

    Add PassportTask

---
+++ PassportTask/PassportApplication/Controllers/PassportsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PassportApplication.Controllers
+{
+    /// <summary>
+    /// Passports API controller
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class PassportsController : ControllerBase
+    {
+        
+    }
+}
---
+++ PassportTask/PassportApplication/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassportApplication.Models;
+
+namespace PassportApplication
+{
+    /// <summary>
+    /// Application setup
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Constructor of Setup
+        /// </summary>
+        /// <param name="configuration">Builder configuration</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Builder configuration
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Services setup
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string? connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        /// <summary>
+        /// Usage setup
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
