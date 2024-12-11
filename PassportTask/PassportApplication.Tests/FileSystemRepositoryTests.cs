cf7a3ee3714ee4a953e5a9183f822d9b4e375daf
AnastasiyaKuzmina <anastasiya.kuzmina.0202@mail.ru>
2024-12-11 12:22:28 +0300

    Add tests

--- PassportTask/PassportApplication.Tests/FileSystemRepositoryTests.cs
+++ PassportTask/PassportApplication.Tests/FileSystemRepositoryTests.cs
@@ -18,11 +18,12 @@ namespace PassportApplication.Tests
             Passports1 = "passports1.txt",
             Passports2 = "passports2.txt",
             PassportsTemplate = "passports_template.txt",
-            PassportsHistory = "passportshistory"
+            PassportsHistory = "passportshistory",
+            FileNameFormat = "dd-MM-yyyy"
         };
 
         [Fact]
-        public async Task GetPassportActivityEnterInvalidSeriesSymbolsReturnFail()
+        public async Task GetPassportActivityEnterInvalidSeriesReturnFail()
         {
             // Arrange
             var mockFormatSettings = new Mock<FormatSettings>();
@@ -35,6 +36,63 @@ namespace PassportApplication.Tests
 
             // Act
             var result = await fileSystemRepository.GetPassportActivityAsync(series, number, cancellationToken);
+
+            // Assert
+            Assert.Equal("Wrong passport format", result.Error?.Message);
+        }
+
+        [Fact]
+        public async Task GetPassportActivityEnterInvalidNumberReturnFail()
+        {
+            // Arrange
+            var mockFormatSettings = new Mock<FormatSettings>();
+            var mockFileSystemDatabase = new Mock<FileSystemDatabase>(MicrosoftOptions.Create(fileSystemSettings));
+            var fileSystemRepository = new FileSystemRepository(mockFormatSettings.Object, mockFileSystemDatabase.Object);
+
+            var cancellationToken = new CancellationToken();
+            string series = "0000";
+            string number = "000d00";
+
+            // Act
+            var result = await fileSystemRepository.GetPassportActivityAsync(series, number, cancellationToken);
+
+            // Assert
+            Assert.Equal("Wrong passport format", result.Error?.Message);
+        }
+
+        [Fact]
+        public async Task GetPassportHistoryEnterInvalidSeriesReturnFail()
+        {
+            // Arrange
+            var mockFormatSettings = new Mock<FormatSettings>();
+            var mockFileSystemDatabase = new Mock<FileSystemDatabase>(MicrosoftOptions.Create(fileSystemSettings));
+            var fileSystemRepository = new FileSystemRepository(mockFormatSettings.Object, mockFileSystemDatabase.Object);
+
+            var cancellationToken = new CancellationToken();
+            string series = "0d00";
+            string number = "000000";
+
+            // Act
+            var result = await fileSystemRepository.GetPassportHistoryAsync(series, number, cancellationToken);
+
+            // Assert
+            Assert.Equal("Wrong passport format", result.Error?.Message);
+        }
+
+        [Fact]
+        public async Task GetPassportHistoryEnterInvalidNumberReturnFail()
+        {
+            // Arrange
+            var mockFormatSettings = new Mock<FormatSettings>();
+            var mockFileSystemDatabase = new Mock<FileSystemDatabase>(MicrosoftOptions.Create(fileSystemSettings));
+            var fileSystemRepository = new FileSystemRepository(mockFormatSettings.Object, mockFileSystemDatabase.Object);
+
+            var cancellationToken = new CancellationToken();
+            string series = "0000";
+            string number = "000d00";
+
+            // Act
+            var result = await fileSystemRepository.GetPassportHistoryAsync(series, number, cancellationToken);
 
             // Assert
             Assert.Equal("Wrong passport format", result.Error?.Message);
@@ -57,6 +115,50 @@ namespace PassportApplication.Tests
 
             // Assert
             Assert.IsType<PassportDto>(result.Value);
+        }
+
+        [Fact]
+        public async Task GetPassportHistoryValidPassportReturnListOfPassportActivityHistoryDto()
+        {
+            // Arrange
+            var mockFormatSettings = new Mock<FormatSettings>();
+            var mockFileSystemDatabase = new Mock<FileSystemDatabase>(MicrosoftOptions.Create(fileSystemSettings));
+            var fileSystemRepository = new FileSystemRepository(mockFormatSettings.Object, mockFileSystemDatabase.Object);
+
+            var cancellationToken = new CancellationToken();
+            string series = "0000";
+            string number = "000000";
+
+            // Act
+            var result = await fileSystemRepository.GetPassportHistoryAsync(series, number, cancellationToken);
+
+            // Assert
+            Assert.IsType<List<PassportActivityHistoryDto>>(result.Value);
+        }
+
+        [Fact]
+        public async Task GetPassportsHistoryInvalidDateReturnFail()
+        {
+            // Arrange
+            var mockFormatSettings = new Mock<FormatSettings>();
+            var mockFileSystemDatabase = new Mock<FileSystemDatabase>(MicrosoftOptions.Create(fileSystemSettings));
+            var fileSystemRepository = new FileSystemRepository(mockFormatSettings.Object, mockFileSystemDatabase.Object);
+
+            var cancellationToken = new CancellationToken();
+            short day = 1;
+            short month = 2;
+            short year = 2003;
+
+            DateOnly date = new DateOnly(year, month, day);
+            var databasePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileSystemSettings.Directory, fileSystemSettings.Database);
+            var passportsHistoryPath = Path.Combine(databasePath, fileSystemSettings.PassportsHistory);
+            string filePath = Path.Combine(passportsHistoryPath, date.ToString(fileSystemSettings.FileNameFormat) + ".txt");
+
+            // Act
+            var result = await fileSystemRepository.GetPassportsChangesForDateAsync(day, month, year, cancellationToken);
+
+            // Assert
+            Assert.Equal($"File {filePath} doesn't exist", result.Error?.Message);
         }
     }
 }
